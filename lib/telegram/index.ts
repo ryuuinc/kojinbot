b199cc0544317ab09063ec46c2d15ad21289cc3c
Tienuon <ryuuinc@gmail.com>
2020-11-13 14:51:27 +0800

    chore: resolve eslint problem and remove generar command

--- lib/telegram/index.ts
+++ lib/telegram/index.ts
@@ -10,20 +10,8 @@ const bot = new Telegraf(process.env.BOT_TOKEN, {
 /* start */
 bot.start((ctx) =>
   ctx.reply(`
-This bot will help you do some stuff. Simply send:
-
-/generar generate Clash config
+This bot will help you do some stuff, but not for now.
 `)
 );
-
-/* generar */
-bot.command('generar', async (ctx) => {
-  const response = await instance.post(process.env.API_URL + '/generar');
-  if (response.data.isDone === true) {
-    ctx.reply('Clash config has been generated.');
-  } else {
-    ctx.reply('Ops! Maybe you need rerun this command.');
-  }
-});
 
 export default bot;
